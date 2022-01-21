58bdae5c120a3e50b93adc0e859e0df94dd4c5a0
Ricardo Hernández Medina <richi3f@gmail.com>
2022-01-21 21:14:38 +0100

    :white_check_mark: Test create Pokémon

--- tests/test_pokedb.py
+++ tests/test_pokedb.py
@@ -1,5 +1,13 @@
 from pokedb import __version__
 
+from pokedb import PokemonDatabase, Pokemon
+
 
 def test_version():
-    assert __version__ == '0.0.0'
+    assert __version__ == "0.0.0"
+
+
+def test_create_pokemon():
+    # Pokémon should be created and added to the database
+    sableye = Pokemon(302, 0)
+    assert PokemonDatabase[302] == sableye
