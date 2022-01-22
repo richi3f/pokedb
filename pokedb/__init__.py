224593fbd855b56e07b10975bf973a6062810a9f
Ricardo Hernández Medina <richi3f@gmail.com>
2022-01-23 00:01:17 +0100

    :sparkles: Add I/O module

--- pokedb/__init__.py
+++ pokedb/__init__.py
@@ -1,5 +1,5 @@
-__all__ = ["core", "Pokemon", "PokemonDatabase"]
+__all__ = ["core", "io", "Pokemon", "PokemonDatabase"]
 __version__ = "0.0.0"
 
-from pokedb import core
+from pokedb import core, io
 from pokedb.pokemon import Pokemon, PokemonDatabase
