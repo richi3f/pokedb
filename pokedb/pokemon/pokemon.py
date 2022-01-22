3b983e9e8c939481f48cb30af24a0e8309221c99
Ricardo Hernández Medina <richi3f@gmail.com>
2022-01-22 04:22:52 +0100

    :sparkles: Add function to convert Pokémon to dictionary

--- pokedb/pokemon/pokemon.py
+++ pokedb/pokemon/pokemon.py
@@ -1,10 +1,10 @@
 __all__ = ["Pokemon"]
 
 import re
-from dataclasses import dataclass, field
+from dataclasses import asdict, dataclass, field
+from enum import Enum
 
 from pokedb.pokemon.database import PokemonDatabase
-
 
 MEGA_PATTERN = re.compile(r"Mega (\w+)(?: X| Y)?")
 
@@ -16,12 +16,11 @@ class Pokemon:
     slug: str = None
     name: str = None
     form_name: str = None
-    egg_groups: tuple = None
+    egg_group: tuple = None
     generation: int = None
-    genders: tuple = None
+    gender: tuple = None
     gender_ratio: int = None
-    types: tuple = None
-    egg_group: str = None
+    pokemon_type: tuple = None
     has_gigantamax: bool = False
     is_sublegendary: bool = False
     is_legendary: bool = False
@@ -34,7 +33,7 @@ class Pokemon:
         self._evolutions = []
 
     @property
-    def evolutions(self) -> tuple:
+    def evolutions(self) -> tuple["Pokemon", ...]:
         return tuple(PokemonDatabase[idx] for idx in self._evolutions)
 
     @property
@@ -44,3 +43,28 @@ class Pokemon:
     def __repr__(self) -> str:
         suffix = "" if self.form_name is None else f" ({self.form_name})"
         return f"{self.name}{suffix}"
+
+    def to_dict(self) -> tuple[dict, list]:
+        # Create Pokémon dictionary
+        pokemon = asdict(self)
+        for key, value in list(pokemon.items()):
+            if isinstance(value, tuple):
+                pokemon.pop(key)
+                pokemon[f"{key}_1"] = str(value[0])
+                if len(value) > 1:
+                    pokemon[f"{key}_2"] = str(value[1])
+            elif isinstance(value, Enum):
+                pokemon[key] = str(value)
+        pokemon["is_mega"] = self.is_mega
+        # Create evolution dictionaries
+        evolutions = []
+        for evolution in self.evolutions:
+            evolutions.append(
+                {
+                    "base_id": self.base_id,
+                    "form_id": self.form_id,
+                    "evo_base_id": evolution.base_id,
+                    "evo_form_id": evolution.form_id,
+                }
+            )
+        return pokemon, evolutions
