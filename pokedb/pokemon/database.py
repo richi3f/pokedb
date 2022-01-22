85b358c3cf726aeb5411ef9be86691556f5b9e74
Ricardo Hernández Medina <richi3f@gmail.com>
2022-01-22 01:23:53 +0100

    :bug: Fix db query function

--- pokedb/pokemon/database.py
+++ pokedb/pokemon/database.py
@@ -8,16 +8,20 @@ class _PokemonDatabase(metaclass=Singleton):
         self._dict = {}
 
     def get_by_slug(self, slug):
-        return self.query(lambda pokemon: pokemon.slug == slug)
+        matches = self.query(lambda pokemon: pokemon.slug == slug)
+        if len(matches) < 1:
+            raise KeyError(f"Pokémon {slug} was not found in the database.")
+        return matches[0]
 
-    def query(self, search_function):
-        return [
-            pokemon for pokemon in self._dict.values() if search_function(pokemon)
-        ]
+    def query(self, search_function) -> list:
+        return [pokemon for pokemon in self._dict.values() if search_function(pokemon)]
 
     def __iter__(self):
         return (
-            pokemon for _, pokemon in sorted(self._dict.items(), key=lambda kvp: (kvp[0][0], kvp[0][1]))
+            pokemon
+            for _, pokemon in sorted(
+                self._dict.items(), key=lambda kvp: (kvp[0][0], kvp[0][1])
+            )
         )
 
     def __getitem__(self, idx):
@@ -28,7 +32,7 @@ class _PokemonDatabase(metaclass=Singleton):
         else:
             raise IndexError("Index must be int or tuple of ints.")
 
-    def __setitem__(self, idx, pokemon):
+    def __setitem__(self, idx, pokemon) -> None:
         if not isinstance(idx, tuple):
             raise IndexError("Index must be a tuple.")
         if pokemon.__class__.__name__ != "Pokemon":
