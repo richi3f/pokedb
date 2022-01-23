36aa44f41d90473ef8c2c9676689a909b37fdb83
Ricardo Hernández Medina <richi3f@gmail.com>
2022-01-23 21:33:27 +0100

    :art: Make use of abstract string enum class

--- pokedb/core/enums/experience.py
+++ pokedb/core/enums/experience.py
@@ -1,19 +1,12 @@
 __all__ = ["ExperienceGroup"]
 
-from enum import Enum
+from pokedb.core.enums.base import StrEnum
 
 
-class ExperienceGroup(Enum):
+class ExperienceGroup(StrEnum):
     ERRATIC = "Erratic"
     FAST = "Fast"
     MEDIUM_FAST = "Medium Fast"
     MEDIUM_SLOW = "Medium Slow"
     SLOW = "Slow"
     FLUCTUATING = "Fluctuating"
-
-    def __str__(self) -> str:
-        return self.value
-
-    @classmethod
-    def from_str(cls, value) -> "ExperienceGroup":
-        return cls._value2member_map_[value]
--- pokedb/core/enums/gender.py
+++ pokedb/core/enums/gender.py
@@ -1,8 +1,5 @@
 __all__ = ["Gender"]
 
-from enum import Enum
-from typing import Any
-
 from pokedb.core.enums.base import StrEnum
 
 
@@ -13,10 +10,3 @@ class Gender(StrEnum):
     MALE_ONLY = "mo"
     FEMALE_ONLY = "fo"
     UNKNOWN = "uk"
-
-    def __str__(self) -> str:
-        return self.value
-
-    @classmethod
-    def from_str(cls, value) -> Any:
-        return cls._value2member_map_[value]
