c64f621d047fbf3c00e602f1d066929bd91380e7
Ricardo Hernández Medina <richi3f@gmail.com>
2022-08-29 14:43:18 +0200

    :sparkles: Crete I/O module

---
+++ src/pokedb/io/js.py
@@ -0,0 +1,20 @@
+__all__ = ["read_js", "to_js"]
+
+import json
+
+from pokedb.core.typing import PathLike
+
+JS_PREFIX = "export default "
+JS_SUFFIX = ";"
+
+
+def read_js(file_path: PathLike) -> dict:
+    with open(file_path, "r", encoding="utf-8") as file:
+        file.seek(len(JS_PREFIX))
+        json_str = file.read()[: -len(JS_SUFFIX)]
+    return json.loads(json_str)
+
+
+def to_js(data: dict, file_path: PathLike, **kwargs) -> None:
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(JS_PREFIX + json.dumps(data, **kwargs) + JS_SUFFIX)
